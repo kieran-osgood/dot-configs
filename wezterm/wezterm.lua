967c7c2f99660c28356dc347f4c9830e7316e540
Kieran Osgood <kieranbosgood@gmail.com>
2024-05-26 22:43:31 +0100

    feat(wezterm): restyle and add re-order commands

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,8 +2,16 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 local mux = wezterm.mux
 
--- config.use_fancy_tab_bar = false
+config.use_fancy_tab_bar = false
+-- config.native_macos_fullscreen_mode = false
 config.native_macos_fullscreen_mode = true
+
+--   Puts tabs in macOS native top menu bar
+-- config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+
+-- Font
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 18.0
 
 -- Color
 local function scheme_for_appearance(appearance)
@@ -11,31 +19,29 @@ local function scheme_for_appearance(appearance)
 		-- config.color_scheme = "AdventureTime"
 		return "Catppuccin Frappe"
 	else
-		return "Catppuccin Latte"
+		return "Catppuccin Frappe"
+		-- return "Catppuccin Latte" // maybe it hurts my brain
 	end
 end
 
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 
--- Font
-config.font = wezterm.font("JetBrains Mono")
-config.font_size = 18.0
-
 -- Window
--- config.window_decorations = "RESIZE"
+config.window_decorations = "RESIZE"
+
 -- https://github.com/letieu/dotfiles/blob/master/dot_config/wezterm/wezterm.lua
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.6
 config.macos_window_background_blur = 40
 
-config.window_frame = {
-	-- The font used in the tab bar.
-	font = wezterm.font({ family = "JetBrains Mono", weight = "Bold" }),
-	font_size = 16.0,
+-- config.window_frame = {
+-- The font used in the tab bar.
+-- font = wezterm.font({ family = "JetBrains Mono", weight = "Bold" }),
+-- font_size = 16.0,
 
-	-- The overall background color of the tab bar when
-	-- the window is focused
-	active_titlebar_bg = "#333333",
-}
+-- The overall background color of the tab bar when
+-- the window is focused
+-- active_titlebar_bg = "#333333",
+-- }
 
 config.window_padding = {
 	left = 0,
@@ -50,54 +56,14 @@ wezterm.on("gui-startup", function()
 	window:gui_window():toggle_fullscreen()
 end)
 
--- config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
-
-config.native_macos_fullscreen_mode = true
 config.keys = {
-	-- Make Option-Left and Option-Right not retarded on macOS. Now that key combo jumps word left or right
-	--
-	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-	-- { key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
-	-- Make Option-Right equivalent to Alt-f; forward-word
-	-- { key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
-	--
-	{
-		key = "LeftArrow",
-		mods = "OPT",
-		action = wezterm.action.SendKey({ key = "b", mods = "ALT" }),
-	},
-	{
-		key = "RightArrow",
-		mods = "OPT",
-		action = wezterm.action.SendKey({ key = "f", mods = "ALT" }),
-	},
-	{
-		key = "|",
-		mods = "CMD|SHIFT",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "-",
-		mods = "CMD|SHIFT",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "w",
-		mods = "CMD",
-		action = wezterm.action.CloseCurrentPane({ confirm = true }),
-	},
-	{
-		key = "p",
-		mods = "CMD",
-		action = wezterm.action.PaneSelect({
-			alphabet = "1234567890",
-		}),
-	},
-	{
-		key = "p",
-		mods = "CMD|SHIFT",
-		action = wezterm.action.ActivateCommandPalette,
-	},
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action.SendKey({ key = "b", mods = "ALT" }) },
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action.SendKey({ key = "f", mods = "ALT" }) },
+	{ key = "|", mods = "CMD|SHIFT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "-", mods = "CMD|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "w", mods = "CMD", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+	{ key = "p", mods = "CMD", action = wezterm.action.PaneSelect({ alphabet = "1234567890" }) },
+	{ key = "p", mods = "CMD|SHIFT", action = wezterm.action.ActivateCommandPalette },
 	{
 		key = "r",
 		mods = "CMD",
@@ -112,7 +78,13 @@ config.keys = {
 	},
 }
 
---   Puts tabs in macOS native top menu bar
-config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+for i = 1, 8 do
+	-- CTRL+ALT + number to move to that position
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "CTRL|ALT",
+		action = wezterm.action.MoveTab(i - 1),
+	})
+end
 
 return config
